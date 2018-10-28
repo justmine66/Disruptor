cfebd0eb97b37992b7d518de89bcc89d4da50941
justmine <justmine@DESKTOP-UILU6SC>
2018-10-28 07:03:41 -0700

    修改方法BitCount

--- src/MDA.Disruptor/Extensions/IntExtension.cs
+++ src/MDA.Disruptor/Extensions/IntExtension.cs
@@ -28,7 +28,7 @@
         {
             if (pos != 0)
             {
-                int mask = int.MaxValue;
+                var mask = int.MaxValue;
                 value = value >> 1;
                 value = value & mask;
                 value = value >> pos - 1;
@@ -38,18 +38,12 @@
         }
 
         /// <summary>
-        /// Returns the number of one-bits in the two's complement binary representation of the specified {@code int} value. This function is sometimes referred to as the <i>population count</i>.
+        /// Returns the number of one-bits in the two's complement binary representation of the specified <paramref name="i"/> value. This function is sometimes referred to as the <i>population count</i>.
         /// </summary>
         /// <param name="i"></param>
         /// <returns></returns>
         public static int BitCount(this int i)
         {
-            i = i - ((i.UnSignedRightShift(1)) & 0x55555555);
-            i = (i & 0x33333333) + ((i.UnSignedRightShift(2)) & 0x33333333);
-            i = (i + (i >> i.UnSignedRightShift(4))) & 0x0f0f0f0f;
-            i = i + (i >> 8);
-            i = i + (i >> 16);
-
             i = i - ((i.UnSignedRightShift(1)) & 0x55555555);
             i = (i & 0x33333333) + ((i.UnSignedRightShift(2)) & 0x33333333);
             i = (i + (i.UnSignedRightShift(4))) & 0x0f0f0f0f;
