383086d8f6dbc8aa7400c312e24300ff719a2b0d
justmine <justmine@DESKTOP-UILU6SC>
2018-10-30 07:42:43 -0700

    添加文件

---
+++ tests/MDA.Disruptor.Test/Ring_Buffer_Event_Matcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace MDA.Disruptor.Test
+{
+    public class Ring_Buffer_Event_Matcher
+    {
+        private readonly object[] _values;
+
+        public Ring_Buffer_Event_Matcher(params object[] values)
+        {
+            _values = values;
+        }
+    }
+}
---
+++ tests/MDA.Disruptor.Test/Ring_Buffer_Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDA.Disruptor.Test
+{
+    public class Ring_Buffer_Test
+    {
+    }
+}
