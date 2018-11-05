8c53c6f4870b4a9e7c9ff4380922abcb1022d1c5
李二毛 <lijun@geekbuy.com.cn>
2018-11-05 10:34:51 +0800

    从MDA迁移到MDA.Disruptor来的第一次提交代码。

---
+++ tests/MDA.Disruptor.ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MDA.Disruptor.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new FalseSharingTest().StartTest();
+            //MonitorTest.Test();
+            //CountdownEventTest.Test();
+            //CountdownEventTest.Test1();
+            //YieldSleep0Sleep1Test.Test();
+
+            Console.Read();
+        }
+    }
+}
