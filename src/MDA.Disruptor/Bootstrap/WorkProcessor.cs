123c026b9ac5f0d1d29bbee90232b70f054bcc1b
李二毛 <lijun@geekbuy.com.cn>
2018-11-06 09:20:25 +0800

    修改命名空间集

--- src/MDA.Disruptor/Bootstrap/WorkProcessor.cs
+++ src/MDA.Disruptor/Bootstrap/WorkProcessor.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading;
-using MDA.Disruptor.Exceptions;
+﻿using MDA.Disruptor.Exceptions;
 using MDA.Disruptor.Impl;
+using System;
+using System.Threading;
 
 namespace MDA.Disruptor.Bootstrap
 {
