26e786c576cabc6a0ec4c1490a0314a618087a11
李二毛 <lijun@geekbuy.com.cn>
2018-11-10 11:51:08 +0800

    修改注释

--- src/MDA.Disruptor/ISequenced.cs
+++ src/MDA.Disruptor/ISequenced.cs
@@ -44,24 +44,23 @@
         long Next(int n);
 
         /// <summary>
-        /// Attempt to claim the next event in sequence for publishing.  Will return the number of the slot if there is at least<code>requiredCapacity</code> slots available.
+        /// Attempt to claim the next event in sequence for publishing. Will return the number of the slot if there is at least<code>requiredCapacity</code> slots available.
         /// </summary>
         /// <param name="sequence">the claimed sequence value</param>
-        /// <returns>true of there is space available in the ring buffer, otherwise false.</returns>
+        /// <returns>true of there is space available in the ring buffer,otherwise false.</returns>
         bool TryNext(out long sequence);
 
         /// <summary>
         /// Attempt to claim the next <code>n</code> events in sequence for publishing.
-        /// Will return the highest numbered slot if there is at least <code>n</code> slots
+        /// Will return the highest numbered slot if there is at least <param name="requiredCapacity"></param> slots
         /// available.
         /// 
-        /// Have a look at <see cref="Next(int)"/> for a description on how to
-        /// use this method.
+        /// Have a look at <see cref="Next(int)"/> for a description on how to use this method.
         /// </summary>
-        /// <param name="n">the number of sequences to claim</param>
-        /// <param name="sequence">the claimed sequence value</param>
+        /// <param name="requiredCapacity">the number of sequences to claim.</param>
+        /// <param name="sequence">the claimed sequence value.</param>
         /// <returns>true of there is space available in the ring buffer, otherwise false.</returns>
-        bool TryNext(int n, out long sequence);
+        bool TryNext(int requiredCapacity, out long sequence);
 
         /// <summary>
         /// Publishes a sequence. Call when the event has been filled.
