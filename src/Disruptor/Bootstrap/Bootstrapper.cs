40dc5464832c7c1b362ee80ac2bb18b179f5552c
李二毛 <lijun@geekbuy.com.cn>
2018-11-19 14:17:30 +0800

    添加Bootstrapper

---
+++ src/Disruptor/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Disruptor.Bootstrap
+{
+    public class Bootstrapper
+    {
+        public static IServiceProvider RegisterServices(
+            ServiceCollection services,
+            ILoggerFactory loggerFactory)
+        {
+            return services.AddLogging()
+                .BuildServiceProvider();
+        }
+    }
+}
