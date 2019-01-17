85429ab3e46625fb1fade12b2311a2f88fabc621
据说甜蜜呢 <lijun@geekbuy.com.cn>
2019-01-17 17:48:12 +0800

    添加文件

---
+++ tests/Disruptor.Test/TimeoutBlockingWaitStrategyTest.cs
@@ -0,0 +1,38 @@
+﻿using Disruptor.Impl;
+using Disruptor.Test.Support;
+using System;
+using Xunit;
+
+namespace Disruptor.Test
+{
+    public class TimeoutBlockingWaitStrategyTest
+    {
+        private readonly ISequenceBarrier _barrier;
+        private readonly IWaitStrategy _strategy;
+        private readonly ISequence _cursor;
+        private readonly ISequence _dependent;
+
+        private const int TimeoutMilliseconds = 1000;
+
+        public TimeoutBlockingWaitStrategyTest()
+        {
+            _barrier = new DummySequenceBarrier();
+            _strategy = new TimeoutBlockingWaitStrategy(TimeoutMilliseconds);
+            _cursor = new Sequence(5);
+            _dependent = _cursor;
+        }
+
+        [Fact(DisplayName = "应该抛出等待超时异常")]
+        public void ShouldTimeoutWaitFor()
+        {
+            var startUtc = DateTime.UtcNow;
+
+            Assert.Throws<TimeoutException>(() => _strategy.WaitFor(6, _cursor, _dependent, _barrier));
+
+            var endUtc = DateTime.UtcNow;
+            var timeWaiting = endUtc.Subtract(startUtc).Ticks;
+
+            Assert.True(timeWaiting >= TimeoutMilliseconds);
+        }
+    }
+}
