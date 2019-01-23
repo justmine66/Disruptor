b0dd67d8c0705db98b78a3ebad6d3dd26044f1f8
justmine <justmine@DESKTOP-UILU6SC>
2019-01-23 21:56:48 +0800

    添加测试方法

--- tests/Disruptor.Test/Dsl/DisruptorTest.cs
+++ tests/Disruptor.Test/Dsl/DisruptorTest.cs
@@ -49,6 +49,50 @@ namespace Disruptor.Test.Dsl
             }
         }
 
+        [Fact]
+        public async Task ShouldBatchOfEvents()
+        {
+            var eventCounter = new CountdownEvent(2);
+            _disruptor.HandleEventsWith(new EventHandler(eventCounter));
+
+            await _disruptor.StartAsync();
+
+            _disruptor.PublishEvents(new EventTranslatorOneArg(_lastPublishedEvent), new object[] { "a", "b" });
+
+            if (!eventCounter.Wait(5))
+            {
+                throw new Exception($"Did not process event published before start was called. Missed events: {eventCounter.CurrentCount}");
+            }
+        }
+
+        [Fact]
+        public void ShouldAddEventProcessorsAfterPublishing()
+        {
+            var rb = _disruptor.GetRingBuffer();
+
+            var b1 = new BatchEventProcessor<TestEvent>(rb, rb.NewBarrier(), new SleepingEventHandler());
+            var b2 = new BatchEventProcessor<TestEvent>(rb, rb.NewBarrier(b1.GetSequence()), new SleepingEventHandler());
+            var b3 = new BatchEventProcessor<TestEvent>(
+             rb, rb.NewBarrier(b2.GetSequence()), new SleepingEventHandler());
+
+            Assert.Equal(b1.GetSequence().GetValue(), -1L);
+            Assert.Equal(b2.GetSequence().GetValue(), -1L);
+            Assert.Equal(b3.GetSequence().GetValue(), -1L);
+
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+
+            _disruptor.HandleEventsWith(b1, b2, b3);
+
+            Assert.Equal(5L, b1.GetSequence().GetValue());
+            Assert.Equal(5L, b2.GetSequence().GetValue());
+            Assert.Equal(5L, b3.GetSequence().GetValue());
+        }
+
         private class EventHandler : IEventHandler<TestEvent>
         {
             private readonly CountdownEvent _counter;
@@ -73,6 +117,21 @@ namespace Disruptor.Test.Dsl
             }
 
             public void TranslateTo(TestEvent @event, long sequence)
+            {
+                _event = @event;
+            }
+        }
+
+        private class EventTranslatorOneArg : IEventTranslatorOneArg<TestEvent, object>
+        {
+            private TestEvent _event;
+
+            public EventTranslatorOneArg(TestEvent lastPublishedEvent)
+            {
+                _event = lastPublishedEvent;
+            }
+
+            public void TranslateTo(TestEvent @event, long sequence, object arg)
             {
                 _event = @event;
             }
