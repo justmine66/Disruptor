4c2aac3fbcfb005938badf55afa8a50342300dd2
justmine <justmine@DESKTOP-UILU6SC>
2019-01-24 22:07:00 +0800

    添加测试方法

--- tests/Disruptor.Test/Dsl/DisruptorTest.cs
+++ tests/Disruptor.Test/Dsl/DisruptorTest.cs
@@ -93,6 +93,115 @@ namespace Disruptor.Test.Dsl
             Assert.Equal(5L, b3.GetSequence().GetValue());
         }
 
+        [Fact]
+        public void ShouldSetSequenceForHandlerIfAddedAfterPublish()
+        {
+            var rb = _disruptor.GetRingBuffer();
+
+            var h1 = new SleepingEventHandler();
+            var h2 = new SleepingEventHandler();
+            var h3 = new SleepingEventHandler();
+
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+
+            _disruptor.HandleEventsWith(h1, h2, h3);
+
+            Assert.Equal(5L, _disruptor.GetSequenceValueFor(h1));
+            Assert.Equal(5L, _disruptor.GetSequenceValueFor(h2));
+            Assert.Equal(5L, _disruptor.GetSequenceValueFor(h3));
+        }
+
+        [Fact]
+        public void ShouldSetSequenceForWorkProcessorIfAddedAfterPublish()
+        {
+            var rb = _disruptor.GetRingBuffer();
+
+            var w1 = CreateTestWorkHandler();
+            var w2 = CreateTestWorkHandler();
+            var w3 = CreateTestWorkHandler();
+
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+            rb.Publish(rb.Next());
+
+            _disruptor.HandleEventsWithWorkerPool(w1, w2, w3);
+
+            Assert.Equal(5L, rb.GetMinimumGatingSequence());
+        }
+
+        [Fact]
+        public async Task ShouldCreateEventProcessorGroupForFirstEventProcessorsAsync()
+        {
+            _executor.IgnoreExecutions();
+
+            var eventHandler1 = new SleepingEventHandler();
+            var eventHandler2 = new SleepingEventHandler();
+
+            var eventHandlerGroup = _disruptor.HandleEventsWith(eventHandler1, eventHandler2);
+
+            await _disruptor.StartAsync();
+
+            Assert.NotNull(eventHandlerGroup);
+            Assert.Equal(2, _executor.ExecutionCount);
+        }
+
+        [Fact]
+        public void ShouldMakeEntriesAvailableToFirstHandlersImmediately()
+        {
+            var countDownLatch = new CountdownEvent(2);
+            var eventHandler = new EventHandlerStub<TestEvent>(countDownLatch);
+
+            _disruptor.HandleEventsWith(CreateDelayedEventHandler(), eventHandler);
+
+            EnsureTwoEventsProcessedAccordingToDependencies(countDownLatch);
+        }
+
+        private async Task EnsureTwoEventsProcessedAccordingToDependencies(CountdownEvent countDownLatch, params DelayedEventHandler[] dependencies)
+        {
+            await PublishEventAsync();
+            await PublishEventAsync();
+
+            foreach (var dependency in dependencies)
+            {
+                Assert.Equal(2, countDownLatch.CurrentCount);
+                dependency.ProcessEvent();
+                dependency.ProcessEvent();
+            }
+
+            AssertThatCountDownLatchIsZero(countDownLatch);
+        }
+
+        private async Task<TestEvent> PublishEventAsync()
+        {
+            if (_ringBuffer == null)
+            {
+                _ringBuffer = await _disruptor.StartAsync();
+                foreach (var eventHandler in _delayedEventHandlers)
+                {
+                    eventHandler.AwaitStart();
+                }
+            }
+
+            _disruptor.PublishEvent(new EventTranslator(_lastPublishedEvent));
+
+            return _lastPublishedEvent;
+        }
+
+        private DelayedEventHandler CreateDelayedEventHandler()
+        {
+            var delayedEventHandler = new DelayedEventHandler();
+            _delayedEventHandlers.Add(delayedEventHandler);
+            return delayedEventHandler;
+        }
+
         private class EventHandler : IEventHandler<TestEvent>
         {
             private readonly CountdownEvent _counter;
@@ -135,6 +244,19 @@ namespace Disruptor.Test.Dsl
             {
                 _event = @event;
             }
+        }
+
+        private TestWorkHandler CreateTestWorkHandler()
+        {
+            var testWorkHandler = new TestWorkHandler();
+            _testWorkHandlers.Add(testWorkHandler);
+            return testWorkHandler;
+        }
+
+        private void AssertThatCountDownLatchIsZero(CountdownEvent countDownLatch)
+        {
+            var released = countDownLatch.Wait(TIMEOUT_IN_SECONDS);
+            Assert.True(released);
         }
     }
 }
---
+++ tests/Disruptor.Test/Dsl/Stubs/EventHandlerStub.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Disruptor.Test.Dsl.Stubs
+{
+    public class EventHandlerStub<T> : IEventHandler<T>
+    {
+        private readonly CountdownEvent _counter;
+
+        public EventHandlerStub(CountdownEvent counter)
+        {
+            _counter = counter;
+        }
+
+        public void OnEvent(T @event, long sequence, bool endOfBatch)
+        {
+            _counter.Signal();
+        }
+    }
+}
